b847472de5744ab5169286ccc6f15bff5675bfd5
arubinofaux <arubinofaux@gmail.com>
2016-04-15 18:33:27 -0400

    some gems

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  slug       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Location < ApplicationRecord
 end
